9dfcddee5da1f49e87fceb61a9c394ad83b69fc0
kaanozcan <knzcnn@gmail.com>
2015-02-24 01:25:57 +0200

    modify to support asynchronous loading views

--- app/templates/app/features/routes/home/home.js
+++ app/templates/app/features/routes/home/home.js
@@ -2,8 +2,24 @@ angular
   .module('app.routes')
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
-      templateUrl: 'app/features/views/home/home.html',
+      templateUrl: 'features/views/home/home.html',
       controller: 'HomeController',
-      controllerAs: 'homeVM'
+      controllerAs: 'homeVM',
+      resolve: {
+        load: ['$http', '$q', '$rootScope', function ($http, $q, $rootScope) {
+          var defer = $q.defer();
+
+          $http.get('features/views/home/home.js')
+            .then(function (response) {
+              eval(response.data);
+              $rootScope.$evalAsync();
+              defer.resolve();
+            }, function(err){
+              defer.reject(err);
+            });
+
+          return defer.promise;
+        }]
+      }
     });
 }]);
