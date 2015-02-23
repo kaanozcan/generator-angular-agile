0170b6d8abd48e5fec931062c9dc1c9105ffb0bc
kaanozcan <knzcnn@gmail.com>
2015-02-24 01:23:26 +0200

    add clean and copy tasks (viewsModule) set angular-builder to ignore app.views

--- app/templates/_GruntFile.js
+++ app/templates/_GruntFile.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
+    clean: ['app/dist'],
     bower_concat: {
       libraries: {
         exclude: [
@@ -13,20 +13,35 @@ module.exports = function (grunt) {
 
     'angular-builder': {
       options: {
-        mainModule: 'app'
+        mainModule: 'app',
+        externalModules: ['ngRoute'],
+        excludedModules: ['app.views']
       },
       app: {
-        src:  ['app/features/**/*.js', '!app/features/views/**/*.js'],
+        src:  ['app/features/**/*.js'],
         dest: 'app/dist/app.js'
+      }
+    },
+
+    copy: {
+      viewsModule: {
+        flatten: true,
+        expand: true,
+        src: 'app/features/views/viewsModule.js',
+        dest: 'app/dist/'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-angular-builder');
 
   grunt.registerTask('default', [
+    'clean',
     'bower_concat',
-    'angular-builder'
+    'angular-builder',
+    'copy'
   ]);
 };
